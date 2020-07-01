1b099c1fe26ab3dc3290aea1a43bef6c58b44e83
Dyriavin <dyriavin@gmail.com>
2020-07-01 13:30:25 +0300

    Crud for Users

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UsersResourse;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return response(['message' => 'Successfully received',
+            'users' => $users], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $validation = Validator::make($input, [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|unique:users',
+            'password' => 'required'
+        ]);
+
+        $user = User::create($validation->validated());
+        return response(['message'=> 'user created','user' => new UsersResourse($user)],201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return response(['message'=> 'success','user' => new UsersResourse($user)],200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $user->update($request->input());
+        return response(['message'=> 'success','user' => new UsersResourse($user)],200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response(['message'=> 'user deleted'],200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,10 @@ use App\Http\Controllers\API\AuthController;
 //});
 Route::post('/register','Api\AuthController@register');
 Route::post('/login', 'Api\AuthController@login');
+Route::middleware('auth:api')->group( function () {
+    Route::resource('users', 'UsersController');
+});
+Route::middleware('auth:api')->post('/users/create','UsersController@store');
+Route::middleware('auth:api')->group(function (){
+    Route::resource('/users','UsersController');
+});
